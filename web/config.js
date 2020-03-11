28c0fa3bc6504e487516047f65b9c352f3a276b0
Diego <topotech@users.noreply.github.com>
2020-03-10 23:29:32 -0300

    Update config.js

--- web/config.js
+++ web/config.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var SPOTIFY_CLIENT_ID = '1c81d0d03de148c083744f5cce782ef7';
+var SPOTIFY_CLIENT_ID = 'c4eda8c2a55049aeaba5ea5905a662db';
 //var REMOTE_SPOTIFY_REDIRECT_URI = 'http://static.echonest.com/OrganizeYourMusic/'
 //var REMOTE_SPOTIFY_REDIRECT_URI = 'http://organizeyourmusic.playlistmachinery.com/'
 var REMOTE_SPOTIFY_REDIRECT_URI = 'https://topotech.github.io/OrganizeYourMusic/web/'
